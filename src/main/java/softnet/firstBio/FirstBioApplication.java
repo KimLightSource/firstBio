bdbaad8791304aacc5f9a31a1f3d69b90b2ea322
DESKTOP-OF69JNK\softnet <qkek875@gmail.com>
2023-02-06 10:55:34 +0900

    Initial commit

---
+++ src/main/java/softnet/firstBio/FirstBioApplication.java
@@ -0,0 +1,13 @@
+package softnet.firstBio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/softnet/firstBio/FirstBioApplicationTests.java
@@ -0,0 +1,13 @@
+package softnet.firstBio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
